03e1d96bdd40906522d9e62c18e263a705f01b7c
ECLIPSE108 <aidanbyrnes@aidandigital.com>
2020-08-26 13:08:11 -0500

    Create mccounter.js

---
+++ js/mccounter.js
@@ -0,0 +1,13 @@
+//based on a pen by @robinselmer
+var url = "https://api.minetools.eu/ping/mc.aidanbyrnes.net/25565";
+ 
+$.getJSON(url, function(r) {
+    //data is the JSON string
+ if(r.error){
+    $('#counter').html('Server Offline');
+   return false;
+ } 
+var pl = '';
+ if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  } 
+  $('#counter').html('<b>Players Online:</b> '+r.players.online);
+});
